0372c88778bc91e0f838d94418c1d9649756acb0
Markus Frosch <markus@lazyfrosch.de>
2016-11-21 22:33:36 +0100

    Add PHP 5.3 based on CentOS 6 packages

--- build-stack.sh
+++ build-stack.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+docker build --rm -t lazyfrosch/icingaweb2-test:5.3 5.3/
 docker build --rm -t lazyfrosch/icingaweb2-test:5.6 5.6/
 docker build --rm -t lazyfrosch/icingaweb2-test:7.0 7.0/
 docker build --rm -t lazyfrosch/icingaweb2-test 7.0/
