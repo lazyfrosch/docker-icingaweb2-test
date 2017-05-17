67ae0e2e4201ace6090d04ab33a557a21d8792f7
Markus Frosch <markus@lazyfrosch.de>
2017-05-17 11:24:08 +0200

    Add 5.4 based on centos

--- build-stack.sh
+++ build-stack.sh
@@ -3,6 +3,7 @@
 set -ex
 
 docker build --rm -t lazyfrosch/icingaweb2-test:5.3 5.3/
+docker build --rm -t lazyfrosch/icingaweb2-test:5.4-centos 5.4-centos/
 docker build --rm -t lazyfrosch/icingaweb2-test:5.6 5.6/
 docker build --rm -t lazyfrosch/icingaweb2-test:7.0 7.0/
 docker build --rm -t lazyfrosch/icingaweb2-test 7.0/
